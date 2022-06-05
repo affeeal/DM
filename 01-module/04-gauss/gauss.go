b03b0005ad318709a602d9204939078efbdc6ec4
affeeal <ilya.afanasyev.26@gmail.com>
2022-06-05 17:24:21 +0300

    gauss.go was fixed

--- 01-module/04-gauss/gauss.go
+++ 01-module/04-gauss/gauss.go
@@ -7,33 +7,13 @@ var (
 	matrix [][]int
 )
 
-// Печать матрицы
-func printMatrix() {
-
-	fmt.Printf("\n")
-	for i := range matrix {
-
-		for j := range matrix[0] {
-
-			if j == N {
-				fmt.Printf("%d\n", matrix[i][j])
-			} else {
-				fmt.Printf("%d ", matrix[i][j])
-			}
-		}
-	}
-}
-
-// Модуль числа
 func abs(a int) int {
-
 	if a > 0 {
 		return a
 	}
 	return -a
 }
 
-// НОД
 func gcd(a, b int) int {
 
 	if b == 0 {
@@ -42,21 +22,16 @@ func gcd(a, b int) int {
 	return gcd(b, a % b)
 }
 
-// НОК
 func lcm(a, b int) int {
 
 	return abs(a * b / gcd(a, b))
 }
 
-// Преобразование столбца матрицы к столбцу диагональной матрицы
 func diagonalizeColumn(I, J int) {
-
 	pivotElem := matrix[I][J]
 	for i := range matrix {
-
 		headElem := matrix[i][J]
 		if headElem != 0 && i != I {
-
 			lcm := lcm(headElem, pivotElem)
 			for j := range matrix[0] {
 
@@ -68,23 +43,31 @@ func diagonalizeColumn(I, J int) {
 	matrix[I], matrix[J] = matrix[J], matrix[I]
 }
 
-func main() {
+func printMatrix() {
+	fmt.Printf("\n")
+	for i := range matrix {
+		for j := range matrix[0] {
+			if j == N {
+				fmt.Printf("%d\n", matrix[i][j])
+			} else {
+				fmt.Printf("%d ", matrix[i][j])
+			}
+		}
+	}
+}
 
+func main() {
 	fmt.Scanf("%d", &N)
 	matrix = make([][]int, N)
 	for i := range matrix {
-
 		matrix[i] = make([]int, N + 1)
 		for j := range matrix[i] {
-
 			fmt.Scanf("%d", &matrix[i][j])
 		}
 	}
 
-	// Приведение матрицы СЛАУ к диагональному виду.
 	for j := range matrix {
 		for i := j; i < N; i++ {
-
 			if matrix[i][j] != 0 {
 				diagonalizeColumn(i, j)
 				break
@@ -92,21 +75,21 @@ func main() {
 		}
 	}	
 
-	// Проверка СЛАУ на совместность.
 	for i := range matrix {
-
 		if matrix[i][i] == 0 {
 			fmt.Printf("No solution\n")
 			return
 		}
 	}
 
-	// Упрощение итоговой СЛАУ и печать решений.
 	for i := range matrix {
-
 		gcd := gcd(matrix[i][i], matrix[i][N])
 		matrix[i][i] /= gcd
 		matrix[i][N] /= gcd
-		fmt.Printf("%d/%d\n", matrix[i][N], matrix[i][i])
+		if matrix[i][N] > 0 && matrix[i][i] < 0 {
+			fmt.Printf("%d/%d\n", -matrix[i][N], -matrix[i][i])
+		} else {
+			fmt.Printf("%d/%d\n", matrix[i][N], matrix[i][i])
+		}
 	}
 }
