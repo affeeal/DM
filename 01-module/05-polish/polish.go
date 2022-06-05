e11e58f9e98434b522a7dd236c939f6cc2ccea17
affeeal <ilya.afanasyev.26@gmail.com>
2022-06-05 17:43:22 +0300

    polish.go was fixed

--- 01-module/05-polish/polish.go
+++ 01-module/05-polish/polish.go
@@ -1,37 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func decodePolish(expr string) int {
-
 	var (
 		pos, len int = 0, len(expr)
 		oStack []byte = make([]byte, 128)
 		nStack []int = make([]int, 128)
 		oCount, nCount int = 0, 0
 	)
-
 	for pos < len {
-
 		sym := expr[pos]
 		if sym >= '0' && sym <= '9' {
-
 			nStack[nCount] = int(sym) - int('0')
 			nCount++
 		} else if sym == '*' || sym == '+' || sym == '-' {
-
 			oStack[oCount] = sym
 			oCount++
 		} else if sym == ')' {
-
 			if oStack[oCount - 1] == '*' {
-
 				nStack[nCount - 2] *= nStack[nCount - 1]
 			} else if oStack[oCount - 1] == '+' {
-
 				nStack[nCount - 2] += nStack[nCount - 1]
 			} else if oStack[oCount - 1] == '-' {
-
 				nStack[nCount - 2] -= nStack[nCount - 1]
 			}
 			oCount--
@@ -43,7 +38,6 @@ func decodePolish(expr string) int {
 }
 
 func main() {
-
-	expr := "(* (* 4 (- (+ (* 2 3) (- 1 4)) 1)) (+ (+ (* 7 8) 4) (* 9 (- 2 8))))"
-	fmt.Printf("%d\n", decodePolish(expr))  //  => 48
+	expr, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Println(decodePolish(expr))
 }
